d96dd2bd2af09e801631c217d905caeacf27cc28
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-20 16:09:40 +0800

    - add flatten

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,23 +1,27 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
     Integer[][] array;
-    public Flaten(Integer[][] array) {
 
+    public Flaten(Integer[][] array) {
+        this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        return Arrays.stream(array)
+                .flatMap(num -> Arrays.stream(num))
+                .collect(Collectors.toList());
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        return Arrays.stream(array)
+                .flatMap(num -> Arrays.stream(num))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
